52f6a6916d70e017cb7d96e2285a7a38e55cec7e
Koussila. ZEKRI <koussila.zekri@oaklandgrp.com>
2025-06-23 17:15:27 +0200

    empty state

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import {Routes} from '@angular/router';
 import {Home} from './home/home';
-import {Login} from './login/login';
 import {AuthGuard} from '@auth0/auth0-angular';
 
 export const routes: Routes = [
@@ -12,11 +11,6 @@ export const routes: Routes = [
   {
     path: 'home',
     component: Home,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'login',
-    component: Login,
     canActivate: [AuthGuard]
   }
 ];
