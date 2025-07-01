f0b48188ce6d0d2e896c8dbad85f4d35ca1bcf14
Koussila. ZEKRI <koussila.zekri@oaklandgrp.com>
2025-07-01 11:38:43 +0200

    my property page setup

--- src/app/lease/edit-property/edit-property.ts
+++ src/app/lease/edit-property/edit-property.ts
@@ -1,17 +1,103 @@
-import { Component } from '@angular/core';
-import {ArrowLeftIcon, LucideAngularModule} from 'lucide-angular';
-import {RouterLink} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ArrowLeftIcon, LucideAngularModule, SaveIcon} from 'lucide-angular';
+import {ActivatedRoute, RouterLink} from '@angular/router';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Property} from '../../../model/property/property';
+import {Lease} from '../../../model/lease/lease';
+import {LeaseRepository} from '../../../repository/lease-repository';
 
 @Component({
   selector: 'app-edit-property',
   imports: [
     LucideAngularModule,
-    RouterLink
+    RouterLink,
+    ReactiveFormsModule,
+    FormsModule
   ],
   templateUrl: './edit-property.html',
   styleUrl: './edit-property.scss'
 })
-export class EditProperty {
+export class EditProperty implements OnInit {
+
+  propertyForm: FormGroup;
+  property: Property | unknown = undefined;
+  heatingTypes = ['ELECTRIC', 'GAS', 'FUEL', 'HEAT_PUMP', 'DISTRICT', 'WOOD', 'SOLAR', 'OTHER'];
+  heatingDistributions = ['RADIATOR', 'UNDERFLOOR', 'AIR', 'OTHER'];
+  propertyTypes = ['APARTMENT', 'HOUSE', 'STUDIO', 'DUPLEX', 'VILLA', 'ROOM', 'COMMERCIAL', 'LAND', 'OTHER'];
+  leaseId: string | null = null;
+  loading = true;
 
   protected readonly ArrowLeftIcon = ArrowLeftIcon;
+
+  constructor(private readonly fb: FormBuilder,
+              private readonly route: ActivatedRoute,
+              private readonly leaseRepository: LeaseRepository) {
+    this.propertyForm = this.fb.group({
+      id: [''],
+      name: ['', Validators.required],
+      type: ['', Validators.required],
+      address: this.fb.group({
+        street: [''],
+        additional: [''],
+        postalCode: [''],
+        city: [''],
+        country: [''],
+      }),
+      surface: this.fb.group({
+        livingArea: [0, Validators.required],
+        totalArea: [null],
+        landArea: [null],
+        balconyArea: [null],
+        terraceArea: [null],
+        gardenArea: [null],
+      }),
+      roomDetails: this.fb.group({
+        rooms: [0, Validators.required],
+        bedrooms: [0, Validators.required],
+        bathrooms: [0, Validators.required],
+        toilets: [0, Validators.required],
+      }),
+      yearOfConstruction: [null],
+      yearOfRenovation: [null],
+      heating: this.fb.group({
+        type: [''],
+        DISTRIBUTION: [''],
+      }),
+      energyLabel: [''],
+      features: this.fb.group({
+        elevator: [false],
+        balcony: [false],
+        terrace: [false],
+        garden: [false],
+        cellar: [false],
+        garage: [false],
+        parking: [false],
+        attic: [false],
+        accessible: [false],
+        intercom: [false],
+        securedDoor: [false],
+        swimmingPool: [false],
+        fireplace: [false],
+        furnished: [false],
+        renovated: [false],
+      }),
+      description: [''],
+      createdAt: [null],
+      updatedAt: [null],
+    });
+  }
+
+  ngOnInit(): void {
+    this.leaseId = this.route.snapshot.paramMap.get('id')
+    if (this.leaseId) {
+      this.leaseRepository.findById(this.leaseId).subscribe((lease: Lease) => {
+        this.property = lease.property;
+        this.loading = false;
+      })
+    }
+  }
+
+  submit() {
+
+  }
 }
--- src/model/shared/address.ts
+++ src/model/shared/address.ts
@@ -1,11 +1,11 @@
 export interface Address {
   street: string;
+  additional: string;
   zipCode: string;
   city: string;
   state: string;
   country: string;
+  building?: string
   floor?: number;
   apartmentNumber?: string;
-  latitude?: number;
-  longitude?: number;
 }
