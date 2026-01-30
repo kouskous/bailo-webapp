05c7c765a6ff0bc11082f257b10bedd5b37c986f
Koussila. ZEKRI <koussila.zekri@oaklandgrp.com>
2026-01-30 12:12:24 +0100

    audience setup + interceptor + token

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,14 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideAuth0} from '@auth0/auth0-angular';
-import {provideHttpClient} from '@angular/common/http';
+import {AuthHttpInterceptor, provideAuth0} from '@auth0/auth0-angular';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
     provideRouter(routes),
     provideAuth0({
       domain: 'bailo.eu.auth0.com',
@@ -17,8 +17,27 @@ export const appConfig: ApplicationConfig = {
       useRefreshTokens: true,
       authorizationParams: {
         redirect_uri: window.location.origin,
-        ui_locales: 'fr'
+        ui_locales: 'fr',
+        audience: 'https://api.bailo.ch',
+      },
+      httpInterceptor: {
+        allowedList: [
+          {
+            uri: 'https://api.bailo.ch/*',
+            tokenOptions: {
+              authorizationParams: {
+                audience: 'https://api.bailo.ch',
+                scope: 'openid profile email'
+              }
+            }
+          }
+        ]
       }
     }),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthHttpInterceptor,
+      multi: true
+    }
   ]
 };
