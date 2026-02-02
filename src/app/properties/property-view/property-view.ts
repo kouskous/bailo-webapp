ba004734fd085d7bc04043b3d9f63b4728ca39f4
Koussila. ZEKRI <koussila.zekri@oaklandgrp.com>
2026-02-02 17:21:11 +0100

    list of properties + edit property + view property

--- src/app/properties/property-view/property-view.ts
+++ src/app/properties/property-view/property-view.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import {ArrowLeftIcon, LucideAngularModule, MapPinIcon, PlusCircleIcon} from 'lucide-angular';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-property-view',
-  imports: [],
+  imports: [
+    LucideAngularModule,
+    RouterLink
+  ],
   templateUrl: './property-view.html',
   styleUrl: './property-view.scss'
 })
 export class PropertyView {
 
+  protected readonly MapPinIcon = MapPinIcon;
+  protected readonly ArrowLeftIcon = ArrowLeftIcon;
+  protected readonly PlusCircleIcon = PlusCircleIcon;
 }
